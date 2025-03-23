eb6ea326eb3ccfd5c43d798caf0e541833196b63
ywywy-b <ywywy0001@gmail.com>
2025-03-23 17:22:31 -0300

    Add files via upload

---
+++ ListaPrimos.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int contador, num;
+    printf("Ingresar un numero: ");
+    scanf("%d", &num);
+
+    printf("Lista de numeros primos hasta %d: \n", num);
+    for (int i = 2; i <= num; i++) {
+        contador = 0;
+        for (int j = 2; j <= i-1; j++) {
+            if (i % j == 0) {
+                contador++;
+                break;
+            }
+        }
+        if (contador == 0) {
+            printf("%d ", i);
+        }
+    }
+    return 0;
+}
